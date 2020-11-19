1bbb190db5bcbbc403204ca9558a0de9f749255e
Tanner <dolb.tanner@gmail.com>
2020-11-18 16:32:33 -0800

    Should be a plugin now

--- .eleventy.js
+++ .eleventy.js
@@ -1,39 +1,33 @@
-module.exports = (eleventyConfig, pluginName) => {
-    // Create shortcode for generating meta tags
-    eleventyConfig.addShortcode("metagen", (data) => {
-
-        if (data) {
-
-            const canonical = `<link rel="canonical" href="${data.url}">`;
-
-            const metadata = `<meta charset="utf-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-                <title>${data.title}</title>
-                <meta name="author" content="${data.name}">\n`.replace(/^\s+/gm, "");
-
-            const openGraph = `
-                <meta name="description" content="${data.desc}">
-                <meta name="og:title" content="${data.title}">
-                <meta name="og:type" content="website">
-                <meta name="og:description" content="${data.desc}">
-                <meta name="og:url" content="${data.url}">
-                <meta name="og:img" content="${data.url.concat(data.img)}">
-                <meta name="og:img:alt" content="${data.img_alt}">\n`.replace(/^\s+/gm, "");
-
-            const twitterCard = `
-                <meta name="twitter:card" content="summary">
-                <meta name="twitter:site" content="${data.twitterHandle}">
-                <meta name="twitter:title" content="${data.title}">
-                <meta name="twitter:description" content="${data.desc}">
-                <meta name="twitter:image" content="${data.url.concat(data.img)}">
-                <meta name="twitter:image:alt" content="${data.img_alt}">\n`.replace(/^\s+/gm, "");
-            
-            console.log(metadata.concat(openGraph, twitterCard, canonical));
-            return metadata.concat(openGraph, twitterCard, canonical);
-
-        } else {
-            console.log("No data was passed into the meta generator!")
-        }
+module.exports = (eleventyConfig, pluginNamespace) => {
+    eleventyConfig.namespace(pluginNamespace, () => {
+        eleventyConfig.addShortcode("metagen", (data) => {
+            if (data) {
+                const canonical = `<link rel="canonical" href="${data.url}">`;
+                const metadata = `<meta charset="utf-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>${data.title}</title>
+                    <meta name="author" content="${data.name}">\n`.replace(/^\s+/gm, "");
+                const openGraph = `
+                    <meta name="description" content="${data.desc}">
+                    <meta name="og:title" content="${data.title}">
+                    <meta name="og:type" content="website">
+                    <meta name="og:description" content="${data.desc}">
+                    <meta name="og:url" content="${data.url}">
+                    <meta name="og:img" content="${data.url.concat(data.img)}">
+                    <meta name="og:img:alt" content="${data.img_alt}">\n`.replace(/^\s+/gm, "");
+                const twitterCard = `
+                    <meta name="twitter:card" content="summary">
+                    <meta name="twitter:site" content="${data.twitterHandle}">
+                    <meta name="twitter:title" content="${data.title}">
+                    <meta name="twitter:description" content="${data.desc}">
+                    <meta name="twitter:image" content="${data.url.concat(data.img)}">
+                    <meta name="twitter:image:alt" content="${data.img_alt}">\n`.replace(/^\s+/gm, "");
+                
+                return metadata.concat(openGraph, twitterCard, canonical);
+            } else {
+                console.log("No data was passed into the meta generator!")
+            }
+        });
     });
 };
