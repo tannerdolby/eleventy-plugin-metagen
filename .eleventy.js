c00486e6dcf16a13c323951e8f2dbbd6a908fd0a
Tanner <tannercdolby@gmail.com>
2022-04-24 17:19:03 -0700

    Remove repetition in .filter and .map calls

--- .eleventy.js
+++ .eleventy.js
@@ -39,38 +39,39 @@ module.exports = (eleventyConfig, pluginNamespace) => {
             if (data) {
                 const openGraphComments = comments(data, "og_comment", "Open Graph");
                 const twitterComments = comments(data, "twitter_comment", "Twitter");
-                const canonical = `\n<link rel="canonical" href="${data.url}">`;
-
+                const canonical = `<link rel="canonical" href="${data.url}">`;
                 const metadata = [
-                    [getTag("meta", null, { charset: get("charset", "utf-8") })], [getTag("meta", null, { "http-equiv": "X-UA-Compatible", content: "IE=edge" })],
-                    ["viewport", "width=device-width, initial-scale=1"], [handleDnsResolution(data)], [getTag("title", data.title)], ["author", data.name],
-                    ["title", data.title], ["description", data.desc], ["robots", data.robots], [handleCustomCrawlers(data)], ["generator", data.generator]
-                ].map((tagInfo, i) => {
-                    if ([0, 1, 3, 4, 9].includes(i)) return tagInfo;
-                    return getTag("meta", null, { name: tagInfo[0], content: tagInfo[1] });
-                }).join("\n");
-
-                const openGraph = openGraphComments.concat("\n", [
-                    ["type", get(data.type, 'website')], ["url", data.url], ["site_name", data.site_name], ["locale", get(data.locale, 'en_US')],
+                    [getTag("meta", null, { charset: get("charset", "utf-8") }), true], [getTag("meta", null, { "http-equiv": "X-UA-Compatible", content: "IE=edge" }), true],
+                    ["viewport", "width=device-width, initial-scale=1"], [handleDnsResolution(data), true], [getTag("title", data.title), true], ["author", data.name],
+                    ["title", data.title], ["description", data.desc], ["robots", data.robots], [handleCustomCrawlers(data), true], ["generator", data.generator]
+                ];
+                const openGraph = [
+                    openGraphComments, ["type", get(data.type, 'website')], ["url", data.url], ["site_name", data.site_name], ["locale", get(data.locale, 'en_US')],
                     ["title", data.og_title || data.title], ["description", data.og_desc || data.desc], ["image", data.img], ["image:alt", data.img_alt],
                     ["image:width", data.img_width], ["image:height", data.img_height]
-                ].map(tagInfo => {
-                    return getTag("meta", null, { property: `og:${tagInfo[0]}`, content: tagInfo[1] });
-                }).join("\n"));
-
-                const twitterCard = twitterComments.concat("\n", [
-                    ["twitter:card", get("twitter_card_type", "foo")], ["twitter:site", `@${data.twitter_handle}`],
+                ];
+                const twitterCard = [
+                    twitterComments, ["twitter:card", get("twitter_card_type", "foo")], ["twitter:site", `@${data.twitter_handle}`],
                     data.twitter_card_type == "summary_large_image" ? ["twitter:creator", `@${get("creator_handle", data.twitter_handle)}`] : "",
                     ["twitter:url", data.url], ["twitter:title", data.twitter_title || data.title], ["twitter:description", data.twitter_desc || data.desc],
                     ["twitter:image", data.img], ["twitter:image:alt", data.img_alt]
-                ].map(tagInfo => {
-                    return getTag("meta", null, { [get("attr_name", "name")]: tagInfo[0], content: tagInfo[1] });
-                }).join("\n"));
-
-                const output = metadata.concat(openGraph, twitterCard, canonical).split("\n");
-                const cleanOutput = output.filter(Boolean).join("\n").replace(/^\s+|[,]$/gm, "");
-
-                return cleanOutput;
+                ];
+                const lengths = [metadata.length, openGraph.length, twitterCard.length];
+                const tags = [...metadata, ...openGraph, ...twitterCard]
+                    .filter(tag => tag[1] != undefined)
+                    .map((tagInfo, i) => {
+                        if (i < lengths[0]) {
+                            if ([0, 1, 3, 4, 9].includes(i)) return tagInfo[0];
+                            return getTag("meta", null, { name: tagInfo[0], content: tagInfo[1] });
+                        } else if (i >= lengths[0] && i < lengths[1] + lengths[2]) {
+                            if (i === lengths[0] || i === lengths[1] + lengths[2] - 1) return tagInfo;
+                            return getTag("meta", null, { property: `og:${tagInfo[0]}`, content: tagInfo[1] });
+                        } else {
+                            return getTag("meta", null, { [get("attr_name", "name")]: tagInfo[0], content: tagInfo[1] });
+                        }
+                    });
+                tags.push(canonical);
+                return tags.filter(Boolean).join("\n").replace(/^\s+|[,]$/gm, "");
             } else {
                 console.error("No data was added into the meta generator")
                 return "";
