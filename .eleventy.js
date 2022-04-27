cc6d48fdec3f4f017549ff89bde4dc059f5a2dfa
Tanner <tannercdolby@gmail.com>
2022-04-26 22:20:12 -0700

    Add features and make logic more maintainable

--- .eleventy.js
+++ .eleventy.js
@@ -3,12 +3,9 @@ const getTag = require("get-tag");
 module.exports = (eleventyConfig, pluginNamespace) => {
     eleventyConfig.namespace(pluginNamespace, () => {
         eleventyConfig.addShortcode("metagen", (data) => {
-            function get(key, fallback) {
-                return data.hasOwnProperty(key) ? data[key] : fallback;
-            }
 
-            function comments(data, field, commentGroup) {
-                return data.comments ? `${(data[field] ? `<!-- ${data[field]} -->` : `<!-- ${commentGroup}-->`)}` : "";
+            function comments(data, key, commentGroup) {
+                return data.comments ? `${(data[key] ? `<!-- ${data[key]} -->` : `<!-- ${commentGroup} -->`)}` : "";
             }
 
             function getDnsTags(data, rel) {
@@ -16,62 +13,115 @@ module.exports = (eleventyConfig, pluginNamespace) => {
                     return `<link rel="${rel}" href="${data}">`
                 } else if (typeof data == "object" && typeof data.crawlers === "object" && data.constructor === Object) {
                     let tags = "";
-                    data.forEach(link => tags += `\n<link rel="${rel}" href="${link}">`);
+                    data.forEach(link => tags += `<link rel="${rel}" href="${link}">`);
                     return tags;
                 }
             }
 
             function handleDnsResolution(data) {
                 let tags = "";
-                if (data.preconnect) tags += "\n" + getDnsTags(data.preconnect, "preconnect");
-                if (data.dns_prefetch) tags += "\n" + getDnsTags(data.dns_prefetch, "dns-prefetch");
+                if (data.preconnect) tags += getDnsTags(data.preconnect, "preconnect");
+                if (data.dns_prefetch) tags += getDnsTags(data.dns_prefetch, "dns-prefetch");
                 return tags;
             }
 
             function handleCustomCrawlers(data) {
                 if (data.crawlers && typeof data.crawlers === "object" && data.constructor === Object) {
                     let tags = "";
-                    for (const key in data.crawlers) tags += `\n<meta name="${key}" content="${data.crawlers[key]}">`;
+                    for (const key in data.crawlers) tags += getTag("meta", null, {name: key, content: data.crawlers[key] });
                     return tags;
                 }
             }
 
-            if (data) {
-                const openGraphComments = comments(data, "og_comment", "Open Graph");
-                const twitterComments = comments(data, "twitter_comment", "Twitter");
-                const canonical = `<link rel="canonical" href="${data.url}">`;
-                const metadata = [
-                    [getTag("meta", null, { charset: get("charset", "utf-8") }), true], [getTag("meta", null, { "http-equiv": "X-UA-Compatible", content: "IE=edge" }), true],
-                    ["viewport", "width=device-width, initial-scale=1"], [handleDnsResolution(data), true], [getTag("title", data.title), true], ["author", data.name],
-                    ["title", data.title], ["description", data.desc], ["robots", data.robots], [handleCustomCrawlers(data), true], ["generator", data.generator]
-                ];
-                const openGraph = [
-                    openGraphComments, ["type", get(data.type, 'website')], ["url", data.url], ["site_name", data.site_name], ["locale", get(data.locale, 'en_US')],
-                    ["title", data.og_title || data.title], ["description", data.og_desc || data.desc], ["image", data.img], ["image:alt", data.img_alt],
-                    ["image:width", data.img_width], ["image:height", data.img_height]
-                ];
-                const twitterCard = [
-                    twitterComments, ["twitter:card", get("twitter_card_type", "foo")], ["twitter:site", `@${data.twitter_handle}`],
-                    data.twitter_card_type == "summary_large_image" ? ["twitter:creator", `@${get("creator_handle", data.twitter_handle)}`] : "",
-                    ["twitter:url", data.url], ["twitter:title", data.twitter_title || data.title], ["twitter:description", data.twitter_desc || data.desc],
-                    ["twitter:image", data.img], ["twitter:image:alt", data.img_alt]
-                ];
-                const lengths = [metadata.length, openGraph.length, twitterCard.length];
-                const tags = [...metadata, ...openGraph, ...twitterCard]
-                    .filter(tag => tag[1] != undefined)
-                    .map((tagInfo, i) => {
-                        if (i < lengths[0]) {
-                            if ([0, 1, 3, 4, 9].includes(i)) return tagInfo[0];
-                            return getTag("meta", null, { name: tagInfo[0], content: tagInfo[1] });
-                        } else if (i >= lengths[0] && i < lengths[1] + lengths[2]) {
-                            if (i === lengths[0] || i === lengths[1] + lengths[2] - 1) return tagInfo;
-                            return getTag("meta", null, { property: `og:${tagInfo[0]}`, content: tagInfo[1] });
+            function getStaticAssets(tagInfoList, asset) {
+                return tagInfoList
+                    .filter(tagInfo => tagInfo[1])
+                    .map(tagInfo => {
+                        if (tagInfo[0] === `inline-${asset}`) {
+                            return asset == "css" ? `\t${getTag("style", tagInfo[1])}` : `\t${getTag("script", tagInfo[1])}`;
                         } else {
-                            return getTag("meta", null, { [get("attr_name", "name")]: tagInfo[0], content: tagInfo[1] });
+                            return tagInfo[1].map(tagData => {
+                                if (typeof tagData === "object") {
+                                    return `<script src="${tagData[0]}" ${tagData[1]}></script>`;
+                                }
+                                return asset == "css" ? getTag("link", null, {rel: "stylesheet", href: tagData }) : `<script src="${tagData}"></script>`;
+                            }).join("\n\t");
                         }
                     });
-                tags.push(canonical);
-                return tags.filter(Boolean).join("\n").replace(/^\s+|[,]$/gm, "");
+            }
+
+            if (data) {
+                const canonical = getTag("link", null, {rel: "canonical", href: data.url });
+                let css, js, customTags = "";
+                const metadata = [
+                    ["charset", getTag("meta", null, {charset: data.charset || "utf-8"})],
+                    ["http-equiv", getTag("meta", null, { "http-equiv": "X-UA-Compatible", content: "IE=edge" })],
+                    ["viewport", "width=device-width, initial-scale=1"],
+                    ["dns", handleDnsResolution(data)],
+                    ["mainTitle", getTag("title", data.title)],
+                    ["author", data.name],
+                    ["title", data.title],
+                    ["description", data.desc],
+                    ["robots", data.robots],
+                    ["crawlers", handleCustomCrawlers(data)],
+                    ["generator", data.generator]
+                ].filter(tagInfo => tagInfo && tagInfo[1]).map(tagInfo => {
+                    if (["charset", "dns", "crawlers", "http-equiv", "mainTitle"].includes(tagInfo[0])) return tagInfo[1];
+                    return getTag("meta", null, { name: tagInfo[0], content: tagInfo[1] });
+                });
+
+                const openGraph = [
+                    ["comments", comments(data, "og-comment", "Open Graph")],
+                    ["og:type", data.type || "website"],
+                    ["og:url", data.url],
+                    ["og:site_name", data.site_name],
+                    ["og:video", data.og_video],
+                    ["og:audio", data.og_audio],
+                    ["og:determiner", data.og_determiner],
+                    ["og:locale:alternate", data.og_alternate_locales],
+                    ["og:locale", data.locale || "en_US"],
+                    ["og:title", data.og_title || data.title],
+                    ["og:description", data.og_desc || data.desc],
+                    ["og:image", data.img],
+                    ["og:image:alt", data.img_alt],
+                    ["og:image:width", data.img_width],
+                    ["og:image:height", data.img_height],
+                    ["og:image:type", data.og_img_type],
+                    ["og:image:secure_url", data.og_img_url]
+                ].filter(tagInfo => tagInfo[1]).map(tagInfo => {
+                    if (tagInfo[0] == "og:locale:alternate") {
+                        return tagInfo[1].map(locale => {
+                            return getTag("meta", null, {property: tagInfo[0], content: locale });
+                        }).join("\n\t");
+                    }
+                    return tagInfo[0] === "comments" ? tagInfo[1] : getTag("meta", null, { property: tagInfo[0], content: tagInfo[1] });
+                });
+                
+                const twitterCard = [
+                    ["comments", comments(data, "twitter-comment", "Twitter")],
+                    ["twitter:card", data.twitter_card_type || 'summary'],
+                    ["twitter:site", `@${data.twitter_handle}`],
+                    data.twitter_card_type === "summary_large_image" ? ["twitter:creator", `@${data.creator_handle || data.twitter_handle}`] : "",
+                    ["twitter:url", data.url],
+                    ["twitter:title", data.twitter_title || data.title],
+                    ["twitter:description", data.twitter_desc || data.desc],
+                    ["twitter:image", data.img],
+                    ["twitter:image:alt", data.img_alt]
+                ].filter(tagInfo => tagInfo[1]).map(tagInfo => {
+                    return tagInfo[0] === "comments" ? tagInfo[1] : getTag("meta", null, { [data.attr_name || "name"]: tagInfo[0], content: tagInfo[1] });
+                });
+
+                if (data.css || data.inline_css) css = getStaticAssets([["css", data.css], ["inline-css", data.inline_css]], "css").join("\n");
+                if (data.js || data.inline_js) js = getStaticAssets([["js", data.js], ["inline-js", data.inline_js]], "js").join("\n");
+
+                if (data.custom) {
+                    customTags = data.custom.map(item => {
+                        if (item.length === 2) return getTag(item[0], "", item[1]);
+                        if (item.length === 3) return getTag(item[0], item[2], item[1]);
+                    }).join("\n\t");
+                }
+
+                return [...metadata, ...openGraph, ...twitterCard, canonical, customTags, css, js].join("\n\t");
             } else {
                 console.error("No data was added into the meta generator")
                 return "";
