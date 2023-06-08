17e086df3b53c919f5f4e9068df82231eff8fca9
Tanner <tannercdolby@gmail.com>
2023-06-07 20:53:08 -0700

    Use array of arrays since an object isn't needed

--- .eleventy.js
+++ .eleventy.js
@@ -131,23 +131,23 @@ module.exports = (eleventyConfig, pluginNamespace) => {
   eleventyConfig.namespace(pluginNamespace, () => {
     eleventyConfig.addShortcode('metagen', (data) => {
       if (data) {
-        const metadata = Object.entries({
-          charset: getTag('meta', null, {
+        const metadata = [
+          ['charset', getTag('meta', null, {
             charset: data.charset || 'utf-8',
-          }),
-          'http-equiv': getTag('meta', null, {
+          })],
+          ['http-equiv', getTag('meta', null, {
             'http-equiv': 'X-UA-Compatible',
             content: 'IE=edge',
-          }),
-          viewport: 'width=device-width, initial-scale=1',
-          title: data.title && getTag('title', data.title),
-          dns: handleDnsResolution(data),
-          author: data.name,
-          description: data.desc || data.description,
-          robots: data.robots,
-          crawlers: handleCustomCrawlers(data),
-          generator: data.generator,
-        })
+          })],
+          ['viewport', 'width=device-width, initial-scale=1'],
+          ['title', data.title && getTag('title', data.title)],
+          ['dns', handleDnsResolution(data)],
+          ['author', data.name],
+          ['description', data.desc || data.description],
+          ['robots', data.robots],
+          ['crawlers', handleCustomCrawlers(data)],
+          ['generator', data.generator],
+        ]
           .filter((tagInfo) => tagInfo[1])
           .map(([tag, val]) =>
             ['charset', 'dns', 'crawlers', 'http-equiv', 'title'].includes(tag)
@@ -158,25 +158,25 @@ module.exports = (eleventyConfig, pluginNamespace) => {
                 }),
           );
 
-        const openGraph = Object.entries({
-          comments: comments(data, 'og-comment', 'Open Graph'),
-          'og:type': data.type || 'website',
-          'og:url': data.url,
-          'og:site_name': data.site_name,
-          'og:video': data.og_video,
-          'og:audio': data.og_audio,
-          'og:determiner': data.og_determiner,
-          'og:locale:alternate': handleAlternateLocales(data.og_alternate_locales),
-          'og:locale': data.locale || 'en_US',
-          'og:title': data.og_title || data.title,
-          'og:description': data.og_desc || data.desc || data.description,
-          'og:image': data.img || data.image,
-          'og:image:alt': data.img_alt || data.image_alt,
-          'og:image:width': data.img_width || data.image_width,
-          'og:image:height': data.img_height || data.image_height,
-          'og:image:type': data.og_img_type || data.og_image_type,
-          'og:image:secure_url': data.og_secure_img_url || data.og_secure_image_url,
-        })
+        const openGraph = [
+          ['comments', comments(data, 'og-comment', 'Open Graph')],
+          ['og:type', data.type || 'website'],
+          ['og:url', data.url],
+          ['og:site_name', data.site_name],
+          ['og:video', data.og_video],
+          ['og:audio', data.og_audio],
+          ['og:determiner', data.og_determiner],
+          ['og:locale:alternate', handleAlternateLocales(data.og_alternate_locales)],
+          ['og:locale', data.locale || 'en_US'],
+          ['og:title', data.og_title || data.title],
+          ['og:description', data.og_desc || data.desc || data.description],
+          ['og:image', data.img || data.image],
+          ['og:image:alt', data.img_alt || data.image_alt],
+          ['og:image:width', data.img_width || data.image_width],
+          ['og:image:height', data.img_height || data.image_height],
+          ['og:image:type', data.og_img_type || data.og_image_type],
+          ['og:image:secure_url', data.og_secure_img_url || data.og_secure_image_url],
+        ]
           .filter((tagInfo) => tagInfo[1])
           .map(([tag, val]) =>
             ['comments', 'og:locale:alternate'].includes(tag)
@@ -184,17 +184,17 @@ module.exports = (eleventyConfig, pluginNamespace) => {
               : getTag('meta', null, { property: tag, content: val }),
           );
 
-        const twitterCard = Object.entries({
-          comments: comments(data, 'twitter-comment', 'Twitter'),
-          'twitter:card': data.twitter_card_type || 'summary',
-          'twitter:site': twitterHandle(data.twitter_handle),
-          'twitter:creator': twitterHandle(data.creator_handle || data.twitter_handle),
-          'twitter:url': data.url,
-          'twitter:title': data.twitter_title || data.title,
-          'twitter:description': data.twitter_desc || data.desc || data.description,
-          'twitter:image': data.img || data.image,
-          'twitter:image:alt': data.img_alt || data.image_alt,
-        })
+        const twitterCard = [
+          ['comments', comments(data, 'twitter-comment', 'Twitter')],
+          ['twitter:card', data.twitter_card_type || 'summary'],
+          ['twitter:site', twitterHandle(data.twitter_handle)],
+          ['twitter:creator', twitterHandle(data.creator_handle || data.twitter_handle)],
+          ['twitter:url', data.url],
+          ['twitter:title', data.twitter_title || data.title],
+          ['twitter:description', data.twitter_desc || data.desc || data.description],
+          ['twitter:image', data.img || data.image],
+          ['twitter:image:alt', data.img_alt || data.image_alt],
+        ]
           .filter((tagInfo) => tagInfo[1])
           .map(([tag, val]) =>
             tag === 'comments'
