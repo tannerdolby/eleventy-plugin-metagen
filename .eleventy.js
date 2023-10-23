ccb14f8dabe374654b8e6b6222a53871e995e043
Tanner <tannercdolby@gmail.com>
2023-10-22 23:12:45 -0700

    feat: format metagen output accordingly

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,14 @@ const metagen = require('meta-generator');
 module.exports = (eleventyConfig, pluginNamespace) => {
   eleventyConfig.namespace(pluginNamespace, () => {
     eleventyConfig.addShortcode('metagen', (data) => {
-      return metagen(data);
+      const head = metagen(data);
+      if (Array.isArray(head)) {
+        return head.join('\n');
+      } else if (typeof head === 'string') {
+        return head;
+      } else {
+        return '';
+      }
     });
   });
 };
